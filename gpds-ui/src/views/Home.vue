8f219ca330c317a5b37ea18d9a56d47c099f2865
Cisnemon13 <20213tn062@utez.edu.mx>
2024-12-16 20:19:52 -0600

    first commit

---
+++ gpds-ui/src/views/Home.vue
@@ -0,0 +1,14 @@
+<!-- src/views/Home.vue -->
+<template>
+    <div>
+      <h1>Home</h1>
+      <p>Bienvenido a la página de inicio nueva </p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage'
+  }
+  </script>
+  
